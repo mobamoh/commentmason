c9b66b83d4ebcc4e396b3656855a828776ee699f
Mohamed Bamoh <mohamed.bamoh@mb-1626.ad.diva-e.com>
2023-09-18 14:36:08 +0200

    implemented db and migrations logic

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/mobamoh/commentmason/internal/db"
+)
 
 // Run is responsible for the instantiation
 // and startup of our application
 func Run() error {
 	fmt.Println("starting up the app...")
+	db, err := db.NewDatabase()
+	if err != nil {
+		return err
+	}
+	if err = db.MigrateDB(); err != nil {
+		return err
+	}
 	return nil
 }
 
---
+++ internal/db/migrate.go
@@ -0,0 +1,32 @@
+package db
+
+import (
+	"fmt"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	_ "github.com/lib/pq"
+)
+
+func (db *Database) MigrateDB() error {
+	fmt.Println("migrating the database")
+	driver, err := postgres.WithInstance(db.Client.DB, &postgres.Config{})
+	if err != nil {
+		return fmt.Errorf("could not create the postgres driver: %w", err)
+	}
+	mig, err := migrate.NewWithDatabaseInstance(
+		"file:///migrations",
+		"postgres",
+		driver,
+	)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	if err := mig.Up(); err != nil {
+		return fmt.Errorf("could not run up migrations: %w", err)
+	}
+	fmt.Println("successfully migrated the database")
+	return nil
+}
